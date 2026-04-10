6a15171739cc42c230bae936d4fc8aa4c7c8f273
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2026-04-10 08:26:22 +0200

    implement local ip adress scan

--- IP_Scanner/Program.cs
+++ IP_Scanner/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace IP_Scanner
+﻿using System.Net;
+
+namespace IP_Scanner
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            static void Main(string[] args)
+            {
+                String strHostName = string.Empty;
+                IPHostEntry ipEntry = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress[] addr = ipEntry.AddressList;
+
+                for (int i = 0; i < addr.Length; i++)
+                {
+                    Console.WriteLine("IP Address {0}: {1} ", i, addr[i].ToString());
+                }
+            }
         }
     }
 }
